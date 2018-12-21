ef346f02d7221031c3ea77bd30f967ca705ac429
Mehdi Drissi <mdrissi@cs.hmc.edu>
2018-12-21 14:27:29 -0800

    Final project paper done

--- validate.sh
+++ validate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 run_classifier.py --data_dir semeval/ --bert_model bert-base-uncased --task_name semeval --output_dir models2/ --max_seq_length 240 --do_eval --do_lower_case --eval_batch_size 16 --model_path trained_model/model.pth
+python3 run_classifier.py --data_dir ../semeval/ --bert_model bert-large-uncased --task_name semeval --output_dir validation_results --max_seq_length 1 --do_eval --do_lower_case --eval_batch_size 16 --model_path trained_large_model/model.pth
